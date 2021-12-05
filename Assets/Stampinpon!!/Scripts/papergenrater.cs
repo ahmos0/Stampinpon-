9f39140978c6c690909b2221b40490b531f6ccf6
ahmos <kamaboko_0228@icloud.com>
2021-12-05 11:01:58 +0900

    debug処理を消去しました

--- Assets/Stampinpon!!/Scripts/papergenrater.cs
+++ Assets/Stampinpon!!/Scripts/papergenrater.cs
@@ -6,9 +6,7 @@ public class papergenrater : MonoBehaviour
 {
     IEnumerator timer()
     {
-        Debug.Log("called");
         yield return new WaitForSeconds(2);
-        Debug.Log("Ok");
         Instantiate(prefab, new Vector3(0.0f, 0.4f, -2.0f), Quaternion.identity);
     }
     public GameObject prefab;
